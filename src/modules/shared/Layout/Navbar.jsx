36a351614ede1ccdcf35003b205c2e88c228c0ce
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-09-05 09:20:05 +0600

    feat: Added Slider/slider1.png, ui/BrandImage.png | refactor: Updated Slider/Slider.jsx, Layout/Navbar.jsx, ui/Brand.jsx

--- src/modules/shared/Layout/Navbar.jsx
+++ src/modules/shared/Layout/Navbar.jsx
@@ -73,7 +73,8 @@ const Navbar = () => {
         ) : (
           <Link
             to="/auth/signin"
-            className="px-4 py-2 rounded-lg bg-[#00BFFF] text-black font-semibold hover:scale-105 hover:shadow-[0_0_12px_#00BFFF] transition-all duration-200"
+            className=" rounded-full btn btn-primary  text-white font-semibold hover:scale-105 hover:shadow-[0_0_12px_#1E3A8A10] transition-all duration-200"
+            // className="btn btn-primary rounded-full"
           >
             Sign In
           </Link>
--- src/modules/shared/ui/Brand.jsx
+++ src/modules/shared/ui/Brand.jsx
@@ -1,20 +1,22 @@
 import { motion } from "motion/react";
 import { useNavigate } from "react-router";
+import BrandImage from "./BrandImage.png";
 
 const Brand = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
+    <div className="flex-center gap-2">
+      <img src={BrandImage} alt="PropBot Estate" className="h-6 w-auto ml-2" />
       <motion.button
         onClick={() => navigate("/")}
-        className="text-xl ml-2"
+        className="text-xl font-bold"
         animate={{
           color: ["#ff5733", "#33ff33", "#8a33ff", "#ce9733"],
           transition: { duration: 9, repeat: Infinity },
         }}
       >
-        PropBot Estate
+        PropBot
       </motion.button>
     </div>
   );
