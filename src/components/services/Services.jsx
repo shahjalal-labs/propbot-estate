191b9e547a2f5ec65ae843577a9d540b20a78740
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-09-06 10:07:14 +0600

    refactor: Updated services/Services.jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -5,34 +5,32 @@ const Services = () => {
     <div>
       <h1>Services</h1>
       <div className="card  max-w-[340px] shadow-sm bg-[#F1F1F1]">
-        <figure>
+        <figure className="p-2 border rounded-xl">
           <img
             src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
             alt="Shoes"
           />
         </figure>
         <div className="card-body">
-          <div className="text-[#979797]">
+          <div className="flex items-center justify-between text-[#979797]">
             <div className="flex items-center gap-1">
               <img src={location} alt="location" />
               <p>New York, NY</p>
             </div>
             <div className="flex items-center gap-1">
-              <img src={star} alt="star" />
+              <img src={star} alt="star" className="" />
               <p>4.5/5</p>
             </div>
           </div>
           <h2 className="card-title">
-            Card Title
-            <div className="badge badge-secondary">NEW</div>
+            Card Title Spacious 3BHK apartment in a prime location with modern
+            amenities.
           </h2>
-          <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
-          </p>
-          <div className="card-actions justify-end">
-            <div className="badge badge-outline">Fashion</div>
-            <div className="badge badge-outline">Products</div>
+          <div className="card-actions items-center justify-between">
+            <button className="btn btn-primary rounded-full px-8 ">
+              Buy Now
+            </button>
+            <span className="text-exl text-black">$450,000</span>
           </div>
         </div>
       </div>
