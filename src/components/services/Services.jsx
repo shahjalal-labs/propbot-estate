8902fb1431a63d1a565ba2f82101c888bb0f8b4f
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-09-06 10:00:14 +0600

    feat: Added services/locationIcon.png, services/starIcon.png | refactor: Updated services/Services.jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,19 +1,35 @@
+import location from "./locationIcon.png";
+import star from "./starIcon.png";
 const Services = () => {
   return (
     <div>
       <h1>Services</h1>
-      <div className="card bg-base-100 w-96 shadow-sm">
+      <div className="card  max-w-[340px] shadow-sm bg-[#F1F1F1]">
         <figure>
           <img
             src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes" />
+            alt="Shoes"
+          />
         </figure>
         <div className="card-body">
+          <div className="text-[#979797]">
+            <div className="flex items-center gap-1">
+              <img src={location} alt="location" />
+              <p>New York, NY</p>
+            </div>
+            <div className="flex items-center gap-1">
+              <img src={star} alt="star" />
+              <p>4.5/5</p>
+            </div>
+          </div>
           <h2 className="card-title">
             Card Title
             <div className="badge badge-secondary">NEW</div>
           </h2>
-          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          <p>
+            A card component has a figure, a body part, and inside body there
+            are title and actions parts
+          </p>
           <div className="card-actions justify-end">
             <div className="badge badge-outline">Fashion</div>
             <div className="badge badge-outline">Products</div>
