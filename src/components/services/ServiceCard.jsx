2427c03c5d8570ab1b50e7e607ae53e9189d60d4
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-09-06 10:14:14 +0600

    feat: Added services/ServiceCard.jsx, services/serviceImage1.png, services/serviceImage2.png, services/serviceImage3.png, services/serviceImage4.png | refactor: Updated services/Services.jsx

---
+++ src/components/services/ServiceCard.jsx
@@ -0,0 +1,32 @@
+const ServiceCard = () => {
+  return (
+    <div className="card  max-w-[340px] shadow-sm bg-[#F1F1F1]">
+      <figure className="p-2">
+        <img src={serviceImage1} alt="Shoes" className="rounded-xl" />
+      </figure>
+      <div className="card-body">
+        <div className="flex items-center justify-between text-[#979797]">
+          <div className="flex items-center gap-1">
+            <img src={location} alt="location" />
+            <p>New York, NY</p>
+          </div>
+          <div className="flex items-center gap-1">
+            <img src={star} alt="star" className="" />
+            <p>4.5/5</p>
+          </div>
+        </div>
+        <h2 className="text-xl font-semibold text-[#222222]">
+          Spacious 3BHK apartment in a prime location with modern amenities.
+        </h2>
+        <div className="card-actions items-center justify-between">
+          <button className="btn btn-primary rounded-full px-8 ">
+            Buy Now
+          </button>
+          <span className="text-xl text-[#222222]">$450,000</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceCard;
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,15 +1,13 @@
 import location from "./locationIcon.png";
+import serviceImage1 from "./serviceImage1.png";
 import star from "./starIcon.png";
 const Services = () => {
   return (
     <div>
       <h1>Services</h1>
       <div className="card  max-w-[340px] shadow-sm bg-[#F1F1F1]">
-        <figure className="p-2 border rounded-xl">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
-          />
+        <figure className="p-2">
+          <img src={serviceImage1} alt="Shoes" className="rounded-xl" />
         </figure>
         <div className="card-body">
           <div className="flex items-center justify-between text-[#979797]">
@@ -22,15 +20,14 @@ const Services = () => {
               <p>4.5/5</p>
             </div>
           </div>
-          <h2 className="card-title">
-            Card Title Spacious 3BHK apartment in a prime location with modern
-            amenities.
+          <h2 className="text-xl font-semibold text-[#222222]">
+            Spacious 3BHK apartment in a prime location with modern amenities.
           </h2>
           <div className="card-actions items-center justify-between">
             <button className="btn btn-primary rounded-full px-8 ">
               Buy Now
             </button>
-            <span className="text-exl text-black">$450,000</span>
+            <span className="text-xl text-[#222222]">$450,000</span>
           </div>
         </div>
       </div>
